f102d5bb903afcd8267cf676194d6b8d627e041b
Nick <shiroaimai@gmail.com>
2021-05-25 15:54:42 +0200

    Init client app and wrapper library

---
+++ FMODPlayer/ConsoleClient/ConsoleClient.cpp
@@ -0,0 +1,9 @@
+// ConsoleClient.cpp : Questo file contiene la funzione 'main', in cui inizia e termina l'esecuzione del programma.
+//
+
+#include <iostream>
+
+int main()
+{
+    std::cout << "Hello World!\n";
+}
---
+++ FMODPlayer/FMODWrapper/FMODWrapper.cpp
@@ -0,0 +1,12 @@
+// FMODWrapper.cpp : Definisce le funzioni per la libreria statica.
+//
+
+#include "pch.h"
+#include "framework.h"
+
+namespace FMODUtil {
+	
+	class Wrapper {
+
+	};
+}
